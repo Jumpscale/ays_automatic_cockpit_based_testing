9e366e6a43be1cb456082e1cc275369f642e31cb
Ramez Saeed <saeedr@codescalers.com>
2016-11-21 18:08:50 +0200

    update config

--- CockpitTesting/Config/generate_config.py
+++ CockpitTesting/Config/generate_config.py
@@ -12,5 +12,7 @@ config.set('main', 'password', cmdargs[3])
 config.set('main', 'account', cmdargs[4])
 config.set('main', 'location', cmdargs[5])
 config.set('main', 'cockpit_url', cmdargs[6])
+config.set('main', 'client_id', cmdargs[7])
+config.set('main', 'client_secret', cmdargs[8])
 with open('config.ini', 'w') as configfile:
     config.write(configfile)
