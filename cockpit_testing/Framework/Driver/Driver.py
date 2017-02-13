624c671bf4c705f9f67c8020a9e52701d547873e
islamTaha12 <tahai@codescalers.com>
2017-02-13 16:50:58 +0200

    Driver support multi test services in the same blueprint

--- cockpit_testing/Framework/Driver/Driver.py
+++ cockpit_testing/Framework/Driver/Driver.py
@@ -9,10 +9,11 @@ import argparse
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', help='use a specific blueprint directory', dest='bpDirectory', default='', action='store')
-    parser.add_argument('-b', help='run list of blueprints name', dest='bpName',  action='store', default=[], nargs='+')
+    parser.add_argument('-b', help='run list of blueprints name', dest='bpName', action='store', default=[], nargs='+')
     parser.add_argument('-a', help='use a specific account', dest='account', default='', action='store')
     parser.add_argument('--no-clone', help='clone development repo', dest='clone', default=True, action='store_false')
-    parser.add_argument('--no-backend', help='no backend environment', dest='no_backend', default=False, action='store_true')
+    parser.add_argument('--no-backend', help='no backend environment', dest='no_backend', default=False,
+                        action='store_true')
     parser.add_argument('--no-teardown', help='no teardown', dest='no_teardown', default=False, action='store_true')
     options = parser.parse_args()
 
@@ -30,19 +31,26 @@ if __name__ == '__main__':
         create_blueprint.create_account()
 
     create_blueprint.create_blueprint()
-    role = {}
+    role = {}  # {'Thread_name : [ [role_name, service], [role_name, service], .. ]}
+
 
     def get_testService_role(blueprint, thread_name):
         global role
+        index = []
         blueprint = blueprint.splitlines()
         for line in blueprint:
-            if 'QA SERVICE' in line:
-                index = blueprint.index(line) + 1
-                break
-        else:
-            raise NameError("The blueprint doesn't have 'QA SERVICE' indicator line")
-        role_line = blueprint[index]
-        role[thread_name] = [role_line[:role_line.find('__')], role_line[role_line.find('__') + 2:-1]]
+            if 'test_' == line[:5]:
+                index.append(blueprint.index(line))
+
+        if len(index) == 0:
+            raise NameError("The blueprint doesn't have any test roles.")
+
+        role[thread_name] = []
+        for i in index:
+            role_line = blueprint[i]
+            role_name = role_line[:role_line.find('__')]
+            role_service = role_line[role_line.find('__') + 2:-1]
+            role[thread_name].append([role_name, role_service])
 
 
     queue = queue.Queue()
@@ -71,20 +79,24 @@ if __name__ == '__main__':
                                                       blueprint=request_cockpit_api.blueprint['name'])
                 request_cockpit_api.run_repository(repository=request_cockpit_api.repo['name'])
 
-                if request_cockpit_api.get_run_status(repository=request_cockpit_api.repo['name'],
-                                                      run_key=request_cockpit_api.repo['key']):
-                    base_test.Testcases_results[
-                        bpFileName] = request_cockpit_api.get_service_data(
-                        repository=request_cockpit_api.repo['name'],
-                        role=role[threading.current_thread().name][0],
-                        service=role[threading.current_thread().name][1])
+                testCase_time = request_cockpit_api.get_run_status(repository=request_cockpit_api.repo['name'],
+                                                                   run_key=request_cockpit_api.repo['key'])
+                if testCase_time:
+                    base_test.Testcases_results[bpFileName] = []
+                    base_test.Testcases_results[bpFileName].append(['TestCase Time', testCase_time])
+                    for role_item in role[threading.current_thread().name]:
+                        base_test.Testcases_results[
+                            bpFileName].append(request_cockpit_api.get_service_data(
+                            repository=request_cockpit_api.repo['name'],
+                            role=role_item[0],
+                            service=role_item[1]))
                 else:
                     request_cockpit_api.testcase_time = '{:0.2f}'.format(time.time() - request_cockpit_api.start_time)
                     error_message = 'ERROR : %s %s' % (
                         request_cockpit_api.blueprint['name'], request_cockpit_api.blueprint['log'])
-
-                    base_test.Testcases_results[bpFileName] = [error_message,
-                                                               request_cockpit_api.testcase_time]
+                    #import ipdb; ipdb.set_trace()
+                    base_test.Testcases_results[bpFileName] = [['TestCase Time', request_cockpit_api.testcase_time],
+                                                               [error_message, role[threading.current_thread().name][0]]]
 
                 queue.task_done()
 
@@ -92,10 +104,11 @@ if __name__ == '__main__':
                 base_test.logging.error(traceback.format_exc())
 
                 # Add error message to xml result
-                error_message = 'ERROR : %s %s' % (request_cockpit_api.response_error_content, traceback.format_exc())
-                base_test.Testcases_results[bpFileName] = [error_message, 0]
+                error_message = 'ERROR : %s %s' % (traceback.format_exc(), request_cockpit_api.response_error_content)
+                base_test.Testcases_results[bpFileName] = [['TestCase Time', 0], [error_message, 'Unknown service']]
 
                 queue.task_done()
+
 
     for _ in range(THREADS_NUMBER):
         threading.Thread(target=work).start()
